141c0907459182ab3a179a490b5cb5925d215834
Sarween <sarweenalages@gmail.com>
2023-09-10 23:07:30 +0800

    backend integration

---
+++ build/agconnect_applinking/generated/source/buildConfig/debug/com/huawei/agc/flutter/applinking/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.huawei.agc.flutter.applinking;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.huawei.agc.flutter.applinking";
+  public static final String BUILD_TYPE = "debug";
+}
